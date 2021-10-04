290d319fd921299d60d3c1af1907fd86cf23dbc4
Thea S. Jepsen <55434874+T-Dawq@users.noreply.github.com>
2021-10-04 14:46:21 +0200

    Nu kan vi også aflæse tekst.

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.androidapp
 
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
-import androidx.core.content.ContextCompat
-import android.Manifest
 import android.content.Intent
 import android.graphics.Bitmap
+import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.Text.TextBlock
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -24,6 +27,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var resultLauncher: ActivityResultLauncher<Intent>
     lateinit var imgView : ImageView
     lateinit var btnScan : Button
+    lateinit var txtViewBlocks : TextView
+    lateinit var txtViewLines : TextView
+    lateinit var txtViewElements : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +37,16 @@ class MainActivity : AppCompatActivity() {
 
         imgView = findViewById(R.id.imageView)
         btnScan = findViewById(R.id.scan_button)
+        txtViewBlocks = findViewById(R.id.txtViewBlocks)
+        txtViewLines = findViewById(R.id.txtViewLines)
+        txtViewElements = findViewById(R.id.txtViewElements)
 
-        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            result ->
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK && result.data != null) {
                 val bundle = result.data!!.extras
                 val bitmap = bundle!!.get("data") as Bitmap
                 imgView.setImageBitmap(bitmap)
+                runTextRecognition(bitmap)
             }
         }
 
@@ -51,5 +60,40 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun runTextRecognition(image: Bitmap) {
+        val image = InputImage.fromBitmap(image, 0)
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+        recognizer.process(image)
+                .addOnSuccessListener { texts ->
+                    processTextRecognitionResult(texts)
+                }
+                .addOnFailureListener { e -> // Task failed with an exception
+                    e.printStackTrace()
+                }
+    }
+
+    private fun processTextRecognitionResult(texts: Text) {
+        val blocks: List<TextBlock> = texts.textBlocks
+        var nBlocks = 0
+        var nLines = 0
+        var nElements = 0
+
+        for (block in blocks) {
+            val lines = block.lines
+            nBlocks++
+            for (line in lines) {
+                val elements = line.elements
+                nLines++
+                for (element in elements) {
+                    nElements++
+                }
+            }
+        }
+
+        txtViewBlocks.append("   $nBlocks")
+        txtViewLines.append("   $nLines")
+        txtViewElements.append("   $nElements")
     }
 }
