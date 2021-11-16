247ab61a5bad9d43d694697cd7874e64189a65a5
LAPTOP-H513JQQ3\sigur <ssan16@student.aau.dk>
2021-11-16 15:22:37 +0100

    insertproductdata er addet

--- app/src/main/java/com/example/androidapp/database/DBManager.kt
+++ app/src/main/java/com/example/androidapp/database/DBManager.kt
@@ -1,5 +1,6 @@
 package com.example.androidapp.database
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,7 +11,17 @@ public class DBManager: SQLiteOpenHelper {
 
     override fun onCreate(db: SQLiteDatabase) {
         createTables(db)
-        insertData(db)
+        insertProductData("Tomat",0.07,0.01,0.0,0.14,0.01,true)
+        insertProductData("Agurk", 0.05,0.01,0.0,0.14,0.01, true)
+        insertProductData("Salat", 0.08, 0.02,0.0,0.06,0.01,false)
+        insertProductData("Rødkål",0.1, 0.02, 0.0,0.06,0.01,false)
+        insertProductData("Æble",0.18,0.02,0.0,0.14,0.01,false)
+        insertProductData("Spidskål",0.1,0.02,0.0,0.06,0.01,false)
+        insertProductData("Blomkål",0.15,0.04,0.0,0.06,0.01,false)
+        insertProductData("Champignon",0.01,0.01,0.0,0.26,0.0,true)
+        insertProductData("Peberfrugt", 0.25,0.03,0.0,0.14,0.01,true)
+        insertProductData("Broccoli",0.15,0.04,0.0,0.06,0.01,false)
+        insertProductData("Gulerod",0.11,0.02,0.0,0.06,0.01,false)
     }
 
     private fun createTables(db: SQLiteDatabase) {
@@ -30,9 +41,24 @@ public class DBManager: SQLiteOpenHelper {
         db.execSQL(createPurchaseTableStmnt)
     }
 
-    private fun insertData(db: SQLiteDatabase) {
-
+    
+    private  fun insertProductData(name: String, cultivation: Double, iluc: Double, processing: Double, packaging: Double, retail: Double,GHCultivated: Boolean ): Long {
+        val db: SQLiteDatabase = this.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put("name", name)
+        contentValues.put("cultivation", cultivation)
+        contentValues.put("iluc", iluc)
+        contentValues.put("processing", processing)
+        contentValues.put("packaging", packaging)
+        contentValues.put("retail", retail)
+        contentValues.put("GHCultivated", GHCultivated)
+        val result: Long = db.insert("product", null, contentValues)
+        return result
     }
+
+
+
+
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
