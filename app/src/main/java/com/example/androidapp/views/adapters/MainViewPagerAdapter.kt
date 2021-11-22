070ba084b635e9b17b8db73644b77804c394cd0d
Thea S. Jepsen <55434874+T-Dawq@users.noreply.github.com>
2021-11-23 00:49:48 +0100

    Har lavet alt for meget uden at pushe

--- app/src/main/java/com/example/androidapp/views/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/androidapp/views/adapters/MainViewPagerAdapter.kt
@@ -2,7 +2,6 @@ package com.example.androidapp.views.adapters
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.androidapp.views.fragments.DataFragment
--- app/src/main/java/com/example/androidapp/views/fragments/EmissionFragment.kt
+++ app/src/main/java/com/example/androidapp/views/fragments/EmissionFragment.kt
@@ -5,12 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.example.androidapp.R
+import com.google.android.material.button.MaterialButtonToggleGroup
 
 class EmissionFragment : Fragment() {
+    private lateinit var toggleButton : MaterialButtonToggleGroup
+    private lateinit var overviewFragment: OverviewFragment
+    private lateinit var listFragment: ListFragment
+    private lateinit var fragmentFL: FrameLayout
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_emission, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emission, container, false)
+        overviewFragment = OverviewFragment()
+        listFragment = ListFragment()
+        toggleButton = rootView.findViewById(R.id.toggleButton)
+        fragmentFL = rootView.findViewById(R.id.fragment_fl)
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        toggleButton.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+            if(isChecked) {
+                when(checkedId) {
+                    R.id.btn_overview -> childFragmentManager.beginTransaction().replace(fragmentFL.id, overviewFragment).commit()
+                    R.id.btn_list -> childFragmentManager.beginTransaction().replace(fragmentFL.id, listFragment).commit()
+                }
+            } else if (toggleButton.checkedButtonId == View.NO_ID) {
+                toggleButton.check(checkedId)
+            }
+        }
+
+        childFragmentManager.beginTransaction().replace(fragmentFL.id, overviewFragment).commit()
+
+        super.onViewCreated(view, savedInstanceState)
     }
 }
