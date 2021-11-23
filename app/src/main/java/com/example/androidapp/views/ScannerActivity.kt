9f074502f3208260ec0eb62fe0bab3528aefe252
LAPTOP-H513JQQ3\sigur <ssan16@student.aau.dk>
2021-11-23 16:01:44 +0100

    exposed drop down

--- app/src/main/java/com/example/androidapp/views/ScannerActivity.kt
+++ app/src/main/java/com/example/androidapp/views/ScannerActivity.kt
@@ -2,11 +2,16 @@ package com.example.androidapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidapp.R
 import com.example.androidapp.views.adapters.RecyclerAdapter
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_scanner.*
+import kotlinx.android.synthetic.main.card_layout.*
 
 
 class ScannerActivity : AppCompatActivity() {
@@ -14,15 +19,33 @@ class ScannerActivity : AppCompatActivity() {
     private var layoutManager: RecyclerView.LayoutManager?=null
     private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanner)
 
+       setupRecyclerView()
+
+       //setupDropdownMenu()
+
+    }
+
+    private fun setupRecyclerView(){
         layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
 
         adapter = RecyclerAdapter()
         recyclerView.adapter = adapter
     }
+
+/*
+    private fun setupDropdownMenu(){
+        //val items = listOf("Option 1", "Option 2", "Option 3", "Option 4")
+        val countries = resources.getStringArray(R.array.countries)
+        val adapter = ArrayAdapter(this, R.layout.dropdown_item, countries)
+        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        autoCompleteTextView.setAdapter(adapter)
+    }
+*/
 }
 
---
+++ app/src/main/java/com/example/androidapp/views/storeItemTitle.kt
@@ -0,0 +1,5 @@
+package com.example.androidapp.views
+
+class storeItemTitle {
+    val name : String = ""
+}
