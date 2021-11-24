ed88e13cdd4ed41f135b5736f703ebd9e28a75bf
Thea S. Jepsen <55434874+T-Dawq@users.noreply.github.com>
2021-11-24 10:57:28 +0100

    Nu henter vi purchases fra databasen

--- app/src/main/java/com/example/androidapp/data/models/Product.kt
+++ app/src/main/java/com/example/androidapp/data/models/Product.kt
@@ -1,4 +1,12 @@
 package com.example.androidapp.data.models
 
-class Product {
+//id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, cultivation REAL NOT NULL, iluc REAL NOT NULL, processing REAL NOT NULL, packaging REAL NOT NULL, retail REAL NOT NULL, GHCultivated BOOLEAN NOT NULL CHECK(GHCultivated IN (0, 1))
+class Product(_name: String, _cultivation: Double, _iluc: Double, _processing: Double, _packaging: Double, _retail: Double, _ghCultivated: Boolean) {
+    val name = _name
+    val cultivation = _cultivation
+    val iluc = _iluc
+    val processing = _processing
+    val packaging = _packaging
+    val retail = _retail
+    val ghCultivated = _ghCultivated
 }
--- app/src/main/java/com/example/androidapp/views/fragments/OverviewFragment.kt
+++ app/src/main/java/com/example/androidapp/views/fragments/OverviewFragment.kt
@@ -1,16 +1,52 @@
 package com.example.androidapp.views.fragments
 
 import android.os.Bundle
+import android.text.SpannableString
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.viewModels
 import com.example.androidapp.R
+import com.example.androidapp.viewmodels.EmissionViewModel
 
 class OverviewFragment : Fragment() {
+    private val viewModel: EmissionViewModel by viewModels({requireParentFragment()})
+    private lateinit var totalEmissionTV: TextView
+    private lateinit var totalEmissionAltTV: TextView
+    private lateinit var emissionReductionTV: TextView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+
+        val rootView = inflater.inflate(R.layout.fragment_overview, container, false)
+
+        totalEmissionTV = rootView.findViewById(R.id.totalEmission)
+        totalEmissionAltTV = rootView.findViewById(R.id.totalEmissionAlt)
+        emissionReductionTV = rootView.findViewById(R.id.percentage)
+
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        viewModel.totalEmission.observe(viewLifecycleOwner, { emission ->
+            val emissionString = "$emission KG CO2"
+            totalEmissionTV.text = emissionString
+        })
+
+        viewModel.totalEmissionAlt.observe(viewLifecycleOwner, { emission ->
+            val emissionString = "$emission KG CO2"
+            totalEmissionAltTV.text = emissionString
+        })
+
+        viewModel.emissionReduction.observe(viewLifecycleOwner, { emissionReduction ->
+            val emissionReductionString = "$emissionReduction %"
+            emissionReductionTV.text = emissionReductionString
+        })
     }
 }
