d053bafbe6b41bd2ce023ddbe88bd616f89cc7e2
Thea S. Jepsen <55434874+T-Dawq@users.noreply.github.com>
2022-03-23 10:54:42 +0100

    Datoer printes pænere og på dansk i listen over indkøb

--- app/src/main/java/com/example/androidapp/models/Trip.kt
+++ app/src/main/java/com/example/androidapp/models/Trip.kt
@@ -1,9 +1,32 @@
 package com.example.androidapp.models
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 class Trip(val purchases: MutableList<Purchase>) {
-    fun prettyTimestamp(): String{
-        return purchases[0].timestamp
+    fun prettyTimestamp(): String {
+        val sdf = SimpleDateFormat("EEE 'd'. d MMM yyyy", Locale.getDefault())
+        var prettyTimestamp = sdf.format(purchases[0].calendar.time)
+        val dayStr = prettyTimestamp.substring(0, 3)
+        val monthStr = prettyTimestamp.substring(prettyTimestamp.length - 8, prettyTimestamp.length)
+
+        prettyTimestamp = prettyTimestamp.replace(dayStr, toDanish(dayStr)).replace(monthStr, toDanish(monthStr))
+
+        return prettyTimestamp
+    }
+
+    private fun toDanish(str: String): String{
+        return when(str) {
+            "Mon" -> "Man"
+            "Tue" -> "Tir"
+            "Wed" -> "Ons"
+            "Thu" -> "Tor"
+            "Fri" -> "Fre"
+            "Sat" -> "Lør"
+            "Sun" -> "Søn"
+            "May" -> "maj"
+            "Oct" -> "okt"
+            else -> str
+        }
     }
 }
